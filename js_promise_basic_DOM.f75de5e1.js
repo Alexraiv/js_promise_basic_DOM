fa40ff46272cb7b241469252d931b6ad2fe23650
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-06-03 17:38:32 +0300

    make build

---
+++ js_promise_basic_DOM.f75de5e1.js
@@ -0,0 +1,34 @@
+'use strict';
+const logo = document.querySelector('.logo');
+const createMessageSuccess = ()=>{
+    const message = document.createElement('div');
+    message.classList.add('message');
+    message.textContent = 'Promise was resolved!';
+    document.body.appendChild(message);
+};
+const createMessageError = ()=>{
+    const errorMessage = document.createElement('div');
+    errorMessage.classList.add('message', 'error-message');
+    errorMessage.textContent = 'Promise was rejected!';
+    document.body.appendChild(errorMessage);
+};
+const promise1 = new Promise((resolve)=>{
+    logo.addEventListener('click', ()=>{
+        resolve();
+    });
+});
+const promise2 = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        reject(new Error());
+    }, 3000);
+});
+Promise.race([
+    promise1,
+    promise2
+]).then(()=>{
+    createMessageSuccess();
+}).catch(()=>{
+    createMessageError();
+});
+
+//# sourceMappingURL=js_promise_basic_DOM.f75de5e1.js.map
